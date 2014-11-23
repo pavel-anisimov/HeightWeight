55ecf5631f9f3f2af4e837ab8a036b0bcadc02c1
Pavel <pavel.n.anisimov@gmail.com>
2014-11-22 17:06:17 -0800

    UI update

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('static-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express      = require('express'),
+    path         = require('path'),
+    favicon      = require('static-favicon'),
+    logger       = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser   = require('body-parser'),
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+    routes = require('./routes/index'),
+    users = require('./routes/users'),
 
-var app = express();
+    app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
